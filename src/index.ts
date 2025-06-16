701e45de94715839709a1359674a9775e0ad64c2
Ifty64bit <ifty64bit@gmail.com>
2025-06-17 02:33:31 +0600

    feat: refactor /relay endpoint to utilize utility functions for translation and messaging

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { Hono } from "hono";
-import { GoogleGenAI } from "@google/genai";
+import {
+    postToFacebook,
+    sendMessageToTelegram,
+    translateText,
+} from "./utils/functions";
 
 type Bindings = {
     TG_BOT_TOKEN: string;
@@ -17,8 +21,6 @@ app.get("/", (c) => {
 
 app.post("/relay", async (c) => {
     try {
-        // const ai = new GoogleGenAI({ apiKey: c.env.GEMINI_API_KEY });
-
         const update = await c.req.json();
 
         const from = update.message.from;
@@ -32,68 +34,23 @@ app.post("/relay", async (c) => {
             return c.text("Invalid message format", 400);
         }
 
-        // const getGeminiResponse = await ai.models.generateContent({
-        //     model: "gemini-2.0-flash",
-        //     contents: `You are a language-cleaning and localization assistant.
-        // Given a short news message copied from a Telegram channel, follow these steps:
-        // 1. Detect and identify the country or countries mentioned (e.g., Russia, Ukraine, Israel, USA).
-        // 2. Add the corresponding flag emoji (🇷🇺, 🇺🇦, 🇮🇱, 🇺🇸, etc.) at the beginning of the final output.
-        // 3. Remove the name or link of the Telegram channel (if mentioned at the beginning or end).
-        // 4. Translate the message into fluent Bangla, using a tone similar to a news headline or update.
-        // 5. Preserve English abbreviations such as ATGM, UAV, IDF, etc.
-        // 6. Use commonly spoken and easy-to-understand Bangla words suitable for social media.
-
-        // ✦ Output only the cleaned Bangla text with the flag emoji at the start. Nothing else.
-
-        // Here is the message:
-        // ${text}`,
-        // });
-
-        // const translatedText = getGeminiResponse.text;
+        const translatedText = await translateText(text, c.env.GEMINI_API_KEY);
 
         //echo the message back
-        const response = {
+        await sendMessageToTelegram({
             chat_id: update.message.chat.id,
             text:
                 `Sender ID: ${from.id}\n` +
                 `From: ${from.first_name} ${from.last_name || ""}\n` +
-                `Message: ${text}\n`,
-        };
-        const url = `https://api.telegram.org/bot${c.env.TG_BOT_TOKEN}/sendMessage`;
-        const fetchOptions = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(response),
-        };
+                `Message: ${translatedText}\n`,
+            TG_BOT_TOKEN: c.env.TG_BOT_TOKEN,
+        });
 
-        const fetchResponse = await fetch(url, fetchOptions);
-
-        if (!fetchResponse.ok) {
-            return c.text("Error sending message", 500);
-        }
-
-        const result = await fetchResponse.json();
-
-        console.log("Response from Telegram:", result);
-
-        // const fbResponse = await fetch(
-        //     `https://graph.facebook.com/${PAGE_ID}/feed`,
-        //     {
-        //         method: "POST",
-        //         headers: { "Content-Type": "application/json" },
-        //         body: JSON.stringify({
-        //             message: translatedText,
-        //             access_token: c.env.FACEBOOK_PAGE_ACCESS_TOKEN,
-        //         }),
-        //     }
-        // );
-
-        // Return a success response
-        if (!result.ok) {
-            return c.text("Failed to send message", 500);
-        }
+        await postToFacebook(
+            translatedText as string,
+            PAGE_ID,
+            c.env.FACEBOOK_PAGE_ACCESS_TOKEN
+        );
 
         return c.text("OK");
     } catch (error) {
---
+++ src/utils/functions.ts
@@ -0,0 +1,84 @@
+import { GoogleGenAI } from "@google/genai";
+
+export async function translateText(
+    text: string,
+    apiKey: string
+): Promise<string | undefined> {
+    const ai = new GoogleGenAI({ apiKey: apiKey });
+    const url = "https://translation.googleapis.com/language/translate/v2";
+    const getGeminiResponse = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: `You are a language-cleaning and localization assistant.
+        Given a short news message copied from a Telegram channel, follow these steps:
+        1. Detect and identify the country or countries mentioned (e.g., Russia, Ukraine, Israel, USA).
+        2. Add the corresponding flag emoji (🇷🇺, 🇺🇦, 🇮🇱, 🇺🇸, etc.) at the beginning of the final output.
+        3. Remove the name or link of the Telegram channel (if mentioned at the beginning or end).
+        4. Translate the message into fluent Bangla, using a tone similar to a news headline or update.
+        5. Preserve English abbreviations such as ATGM, UAV, IDF, etc.
+        6. Use commonly spoken and easy-to-understand Bangla words suitable for social media.
+
+        ✦ Output only the cleaned Bangla text with the flag emoji at the start. Nothing else.
+
+        Here is the message:
+        ${text}`,
+    });
+
+    return getGeminiResponse.text;
+}
+
+type SendMessageToTelegramType = {
+    chat_id: string;
+    text: string;
+    TG_BOT_TOKEN?: string;
+};
+export async function sendMessageToTelegram({
+    chat_id,
+    text,
+    TG_BOT_TOKEN,
+}: SendMessageToTelegramType) {
+    const url = `https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage`;
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            chat_id,
+            text,
+        }),
+    };
+
+    const fetchResponse = await fetch(url, fetchOptions);
+
+    if (!fetchResponse.ok) {
+        throw new Error("Error sending message");
+    }
+
+    return fetchResponse.json();
+}
+
+export async function postToFacebook(
+    text: string,
+    PAGE_ID: string,
+    token: string
+) {
+    const url = `https://graph.facebook.com/${PAGE_ID}/feed`;
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            message: text,
+            access_token: token,
+        }),
+    };
+
+    const fetchResponse = await fetch(url, fetchOptions);
+
+    if (!fetchResponse.ok) {
+        throw new Error("Error posting to Facebook");
+    }
+
+    return fetchResponse.json();
+}
